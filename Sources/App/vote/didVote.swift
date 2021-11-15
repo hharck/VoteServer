755fff19a38b4ab4c04934b4ee9d3bc72388555c
Hans Harck Tønning <hharck2@gmail.com>
2021-11-15 19:55:35 +0100

    Added missing files to git

---
+++ Sources/App/vote/didVote.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Hans Harck Tønning on 14/11/2021.
+//
+
+import Foundation
+struct DidVote: Codable{
+	var title: String
+	var voterID: String
+	var priorities: [String]
+}
