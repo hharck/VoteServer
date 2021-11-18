03b6da7fbab7684111556f2f2b975aaa68bcb748
Hans Harck Tønning <hharck2@gmail.com>
2021-11-18 15:31:51 +0100

    Added a version page and a Digital Ocean referral badge in the corner of some pages

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,6 +8,10 @@ func routes(_ app: Application) throws {
 	app.get { req in
 		req.redirect(to: "/create/")
 	}
+	
+	app.get("version"){req async in
+		return "Server version: 0.1.0\nAltVoteKit version \(AltVoteKitVersion)"
+	}
 
 	try voteCreationRoutes(app, voteManager: voteManager)
 	try votingRoutes(app, voteManager: voteManager)
